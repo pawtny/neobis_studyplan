da8a573f769c84ea39fd2e3c7dcae08471a2ae3b
pawtny <sultanaliev_s@auca.kg>
2020-06-13 19:46:46 +0600

    MUDA

--- Vychisleniya_PloshadIPerimetr.py
+++ Vychisleniya_PloshadIPerimetr.py
@@ -6,5 +6,4 @@ a = math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
 b = math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3))
 c = math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3))
 p = a + b + c
-print(p)
-print(math.sqrt(p / 2 * (p / 2 - a) * (p / 2 - b)* (p / 2 - c)))
+print(p, math.sqrt(p / 2 * (p / 2 - a) * (p / 2 - b)* (p / 2 - c)))
